bcb5987c4c5abc213d43164b347bce7bfaaebd98
Sahil Dua <sahildua2305@gmail.com>
2016-01-04 23:08:39 +0530

    Modified admin.py

--- sukublog/admin.py
+++ sukublog/admin.py
@@ -3,10 +3,8 @@
 # @Author: sahildua2305
 # @Date:   2016-01-01 19:27:32
 # @Last Modified by:   sahildua2305
-# @Last Modified time: 2016-01-02 01:26:22
+# @Last Modified time: 2016-01-04 23:08:22
 
 from django.contrib import admin
-
-from mongoengine import *
 
 from sukublog.models import BlogPost, ImagePost, VideoPost, QuotePost, LinkPost
